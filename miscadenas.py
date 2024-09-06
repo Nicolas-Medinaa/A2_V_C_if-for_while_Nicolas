a29e1e1297e4737e953780985a66ea77bf3d3081
Nicolas Medina Zubia <a.22308051281259@cbtis128.edu.mx>
2024-09-06 09:58:43 -0600

    Add files via upload

--- miscadenas.py
+++ miscadenas.py
@@ -1,7 +1,19 @@
-nombre="Reyes"
+nombre="Nicolas"
 print(nombre[0])
 print(nombre)
 
-MisNovias=["E","l","i"]
+MisNovias=["e","r","o"]
 for chicas in MisNovias:
     print(chicas)
+
+    print("otro ejemplo")
+    b = "Nicolas Medina"
+print (b[0:7])
+print ("a mayusculas")
+a = "Nikocrak"
+print(a.upper())
+print("concatecar")
+a = "rosa "
+b = "melano"
+c = a + b
+print(c)
