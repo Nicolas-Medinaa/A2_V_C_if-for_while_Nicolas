9ae284f2fb2290d1dc58e9138f356e9c0a73732c
Nicolas Medina Zubia <a.22308051281259@cbtis128.edu.mx>
2024-09-06 09:57:47 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,7 @@
+nombre="Reyes"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["E","l","i"]
+for chicas in MisNovias:
+    print(chicas)
---
+++ misvariables.py
@@ -0,0 +1,15 @@
+import math
+edad=17   # 17 se gurada en edad
+nombre="Reyes Ramirez"
+print("tu edad es ",edad)
+print("te llamas ",nombre)
+print("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.14159)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funcnes matematicas ---")
+x = math.sqrt(64)
+print(x)
